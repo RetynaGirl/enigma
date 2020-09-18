027dcc3f7e7a7f46d40d178a2cecd1ddbc79844f
Aidan Murray <aidanhansm@gmail.com>
2020-09-18 11:01:33 -0600

    Add test for encrypt

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -11,4 +11,8 @@ class EnigmaTest < Minitest::Test
   def test_decrypt
     assert_equal 'applied_message', @enigma.decrypt('foo', :key, :date)
   end
+
+  def test_encrypt
+    assert_equal 'applied_message', @enigma.encrypt
+  end
 end
