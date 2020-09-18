b76468838ff3a4e274ce79afbf70e0ec43f5c614
Aidan Murray <aidanhansm@gmail.com>
2020-09-18 10:49:12 -0600

    Complete decrypt method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -2,7 +2,9 @@
 
 # Runs all code in order and stuff
 class Enigma
-  def decrypt(_cyphertext, _key, _date)
-    decrypted
+  def decrypt(cyphertext, key, date)
+    decryption_key = KeyGen.decryption_key(key, date)
+
+    KeyApplier.call(cyphertext, decryption_key)
   end
 end
