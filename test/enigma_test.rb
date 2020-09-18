ca102e0578380b9a1558219dffe9507205ce5e45
Aidan Murray <aidanhansm@gmail.com>
2020-09-18 11:42:45 -0600

    Update tests for decrypt and encrypt

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -1,18 +1,20 @@
 require './test/test_helper'
+# require './lib/key_gen'
 
 class EnigmaTest < Minitest::Test
   def setup
-    KeyGen.stubs(:decryption_key).returns('a')
-    KeyGen.stubs(:encryption_key).returns('b')
+    # require 'pry'; binding.pry
+    KeyGen.stubs('decryption_key').returns('a')
+    KeyGen.stubs('encryption_key').returns('b')
     KeyApplier.stubs(:apply_key).returns('applied message')
     @enigma = Enigma.new
   end
 
   def test_decrypt
-    assert_equal 'applied_message', @enigma.decrypt('foo', :key, :date)
+    assert_equal 'applied message', @enigma.decrypt('foo', :key, :date)
   end
 
   def test_encrypt
-    assert_equal 'applied_message', @enigma.encrypt
+    assert_equal 'applied message', @enigma.encrypt('foo', :key, :date)
   end
 end
