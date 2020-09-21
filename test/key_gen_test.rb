ea19a2349fe7b3bb23b97b351f61e74675af67da
Aidan Murray <aidanhansm@gmail.com>
2020-09-21 12:37:33 -0600

    Add test for make offsets

--- test/key_gen_test.rb
+++ test/key_gen_test.rb
@@ -8,4 +8,8 @@ class KeyGenTest < Minitest::Test
   def test_make_keys
     assert_equal [2, 27, 71, 15], KeyGen.make_keys('02715')
   end
+
+  def test_make_offsets
+    assert_equal [1, 0, 2, 5], KeyGen.make_offsets('040895')
+  end
 end
