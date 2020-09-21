32f20c07e0949aab81ee2c1e330578b68d5b9e86
Aidan Murray <aidanhansm@gmail.com>
2020-09-21 12:38:07 -0600

    Add encrypt method

--- lib/key_gen.rb
+++ lib/key_gen.rb
@@ -2,4 +2,9 @@
 
 # Creates encryption and decryption keys
 class KeyGen
+  def self.encrypt(key, date)
+    keys = make_keys(key)
+    offsets = make_offsets(date)
+    keys.map.with_index { |key, idx| key + offsets[idx].to_i }
+  end
 end
