ea932af18ceb66f7824fc375013e20e8cf793417
Aidan Murray <aidanhansm@gmail.com>
2020-09-21 12:36:15 -0600

    Add test for make keys

--- test/key_gen_test.rb
+++ test/key_gen_test.rb
@@ -4,4 +4,8 @@ class KeyGenTest < Minitest::Test
   def test_encrypt
     assert_equal [3, 27, 73, 20], KeyGen.encrypt('02715', '040895')
   end
+
+  def test_make_keys
+    assert_equal [2, 27, 71, 15], KeyGen.make_keys('02715')
+  end
 end
