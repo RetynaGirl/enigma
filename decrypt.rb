fdb59b203a1a25abd05193da6e1074cd80d6ef6d
Aidan Murray <aidanhansm@gmail.com>
2020-09-21 17:17:23 -0600

    Update decrypt for actually working files

--- decrypt.rb
+++ decrypt.rb
@@ -6,4 +6,8 @@ ciphertext = File.new(ARGV[0]).read
 
 decrypted = Enigma.new.decrypt(ciphertext, ARGV[2], ARGV[3])
 
-File.new(ARGV[1]).write(decrypted)
+decrypted_file = File.new(ARGV[1], 'w')
+decrypted_file.write(decrypted)
+decrypted_file.close
+
+puts "Created '#{ARGV[0]}' with the key #{ARGV[2]} and date #{ARGV[3]}"
