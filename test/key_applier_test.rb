2771d529fec754157a559a9f5c3724e0bd807734
Aidan Murray <aidanhansm@gmail.com>
2020-09-21 12:55:54 -0600

    Add test for wrap apply

--- test/key_applier_test.rb
+++ test/key_applier_test.rb
@@ -4,4 +4,8 @@ class KeyApplierTest < Minitest::Test
   def test_apply_key
     assert_equal 'keder ohulw', KeyApplier.apply_key('hello world', [3, 0, 19, 20])
   end
+
+  def test_wrap_apply
+    assert_equal 'b', KeyApplier.wrap_apply('z', 2)
+  end
 end
