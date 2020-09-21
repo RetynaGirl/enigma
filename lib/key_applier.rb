e60176827a35efb6dd34ff88cba6e383bd5e5ab6
Aidan Murray <aidanhansm@gmail.com>
2020-09-21 12:54:51 -0600

    Add apply key method

--- lib/key_applier.rb
+++ lib/key_applier.rb
@@ -2,4 +2,9 @@
 
 # Applies key hashes to incoming messages
 class KeyApplier
+  def self.apply_key(message, key)
+    message.downcase.map.with_index do |character, idx|
+      character.wrap_apply(character, key[idx % 4])
+    end
+  end
 end
